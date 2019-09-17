28bea5dbd8a953a1da56011caf490dd49beaf9d8
Maasa <konomaasa@gmail.com>
2019-09-17 06:50:07 -0700

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,24 @@
-require 'pry'
 class PigLatinizer
     attr_reader :words
 
-    def initialize(words)
-        @words = words.downcase
-    end
+    # def initialize(words)
+    #     @words = words
+    # end
 
-    def piglatinize 
+    def piglatinize(words)
         word = words.split(" ")
         result = word.map do |w|
-            binding.pry
-            first = w.split /([aeiouAEIOU].*)/
-            first
+            if w[0].match(/a|e|i|o|u|y|A|E|I|O|U|Y/)
+                final = w << "way"
+            else 
+                sep = w.split(/([aeiouyAEIOUY].*)/)
+                first = sep[0]
+                last = sep[1]
+                final = last << first << "ay"
+                final
+            end
         end
-        result
+        result.join(" ")
     end
 
 end
