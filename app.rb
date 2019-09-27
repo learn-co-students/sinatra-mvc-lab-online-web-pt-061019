74ad3f36e3dac146a9bc5a2000c01db73b1d6842
Oscar <shoudto@gmail.com>
2019-09-26 20:41:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,11 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
     get '/' do 
+        erb :user_input
     end 
 
     post '/piglatinize' do 
-    end 
-
-    post '/piglatinize' do 
+        erb :results
     end 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,28 @@
+require 'pry'
+
 class PigLatinizer
     
     attr_accessor :word
 
-    def piglatinize(word) 
+    def piglatinize(word)
 
+        vowels = ["a", "e", "i", "o", "u"]
+
+        if word.include?(" ")
+            split_text(word).join(" ")
+        elsif vowels.include?(word[0].downcase)
+            word + "way"
+        else 
+            words = word.split(/[aeiou].*/)
+            words[1]+words[0]+"ay"
+        end 
     end
+
+    def split_text(word)
+        word_array = word.split(" ")
+
+        word_array.map do |word|
+            piglatinize(word)
+        end 
+    end 
 end 
