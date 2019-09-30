9c40e99d173e2e850948cb4322052034e7b102d5
Oscar <shoudto@gmail.com>
2019-09-30 00:08:43 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,24 +5,6 @@ class PigLatinizer
     attr_accessor :word
 
     def piglatinize(word)
-
-        vowels = ["a", "e", "i", "o", "u"]
-
-        if word.include?(" ")
-            split_text(word).join(" ")
-        elsif vowels.include?(word[0].downcase)
-            word + "way"
-        else 
-            words = word.split(/[aeiou].*/)
-            words[1]+words[0]+"ay"
-        end 
-    end
-
-    def split_text(word)
-        word_array = word.split(" ")
-
-        word_array.map do |word|
-            piglatinize(word)
-        end 
+        binding.pry
     end 
 end 
